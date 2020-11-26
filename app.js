016672d951338f3caf96ead792fd53273ea89661
Daniel Clarke <daniel@danielrlc.com>
2020-11-26 02:12:08 +0200

    manufacturers list built using async, await and fetch

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-var displayedData = document.querySelector("#displayedData");
-var listOfProducts = ["jackets", "shirts", "accessories"];
+var displayedData = document.querySelector('#displayedData');
+var listOfProducts = ['jackets', 'shirts', 'accessories'];
 
-var getListOfManufacturersForAProduct = async function(product) {
+var buildManufacturersList = async function (product) {
   var list = [];
-  productUrl = "https://bad-api-assignment.reaktor.com/products/" + product;
-  await fetch(productUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      data
-        .map(function (item) {
-          var manufacturer = item.manufacturer;
-          var manufacturerIsNew = !list.includes(manufacturer);
-          if (manufacturerIsNew) {
-            list = list.concat(manufacturer);
-          }
-        });
+  productUrl = 'https://bad-api-assignment.reaktor.com/products/' + product;
+  try {
+    // Handle success case
+    const response = await fetch(productUrl);
+    const inventory = await response.json();
+    inventory.map((item) => {
+      var manufacturer = item.manufacturer;
+      var manufacturerIsNew = !list.includes(manufacturer);
+      if (manufacturerIsNew) {
+        list = list.concat(manufacturer);
+      }
     });
-  return list;
-}
+  } catch (error) {
+    // Handle error case
+    console.log(error);
+  }
+  console.log(list);
+};
 
-var listOfManufacturersForJackets = getListOfManufacturersForAProduct("jackets");
-
-console.log(listOfManufacturersForJackets)
+var jacketManufacturerList = buildManufacturersList('jackets');
