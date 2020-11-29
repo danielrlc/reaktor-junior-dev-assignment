d59e08996e402c2a3ca1dfead37de3e8da902a96
Daniel Clarke <daniel@danielrlc.com>
2020-11-29 16:42:32 +0200

    List of availability values for reps manufacturer

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var displayedData = document.querySelector('#displayedData');
+const displayedItems = document.querySelector('#displayedItems');
 
-var buildManufacturerList = async function (product) {
-  var manufacturerList = [];
-  var inventory = [];
-  var productUrl = 'https://bad-api-assignment.reaktor.com/products/' + product;
+const buildManufacturerList = async function (product) {
+  let manufacturerList = [];
+  let repsInventory = [];
+  let inventory = [];
+  const productUrl =
+    'https://bad-api-assignment.reaktor.com/products/' + product;
   try {
     // Handle success case
     const response = await fetch(productUrl);
     inventory = await response.json();
+    console.log(inventory);
     inventory.map((item) => {
-      var manufacturer = item.manufacturer;
-      var manufacturerIsNew = !manufacturerList.includes(manufacturer);
+      const manufacturer = item.manufacturer;
+      const manufacturerIsNew = !manufacturerList.includes(manufacturer);
       if (manufacturerIsNew) {
         manufacturerList = manufacturerList.concat(manufacturer);
       }
@@ -22,9 +25,51 @@ var buildManufacturerList = async function (product) {
   runApp(inventory, manufacturerList);
 };
 
+const buildAvailabilityList = async function (manufacturer) {
+  let availabilityCategories = [];
+  const availabilityUrl =
+    'https://bad-api-assignment.reaktor.com/availability/' + manufacturer;
+  try {
+    // Handle success case
+    const response = await fetch(availabilityUrl);
+    const responseData = await response.json();
+    const availabilityData = responseData.response;
+    console.log(availabilityData);
+    availabilityData
+      .filter((_, i) => i < 10)
+      .map((item) => {
+        const inStockValue = item.DATAPAYLOAD.substring(31).split('<')[0];
+        console.log(inStockValue);
+      });
+  } catch (error) {
+    // Handle error case
+    console.log(error);
+  }
+};
+
 buildManufacturerList('jackets');
+buildAvailabilityList('reps');
 
 function runApp(inventory, manufacturerList) {
-  console.log(manufacturerList);
-  console.log(inventory);
+  const tenJ = inventory.filter((item, i) => i < 10);
+  console.log(tenJ);
+  tenJ.map((item, i) => {
+    const tr = document.createElement('tr');
+    const id = document.createElement('td');
+    const name = document.createElement('td');
+    const manufacturer = document.createElement('td');
+    const color = document.createElement('td');
+    const price = document.createElement('td');
+    id.textContent = item.id;
+    name.textContent = item.name;
+    manufacturer.textContent = item.manufacturer;
+    color.textContent = item.color;
+    price.textContent = item.price;
+    tr.appendChild(id);
+    tr.appendChild(name);
+    tr.appendChild(manufacturer);
+    tr.appendChild(color);
+    tr.appendChild(price);
+    displayedItems.appendChild(tr);
+  });
 }
